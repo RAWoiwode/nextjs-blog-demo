f95e122154948f608cce661d6b5a6cc3e45009d6
Ralph Woiwode <rawoiwode@gmail.com>
2024-09-17 13:38:55 -0500

    Creating components for posts and adding dummy post data

--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,42 @@
-import Hero from "@/components/Home/hero";
+import FeaturedPosts from "@/components/home/featured-posts";
+import Hero from "@/components/home/hero";
+
+const DUMMY_POSTS = [
+  {
+    title: "Getting Started",
+    slug: "getting-started",
+    image: "getting-started.png",
+    excerpt: "NextJS is the React Framework for production",
+    date: "2024-04-16",
+  },
+  {
+    title: "Getting Started",
+    slug: "getting-started2",
+    image: "getting-started.png",
+    excerpt: "NextJS is the React Framework for production",
+    date: "2024-04-16",
+  },
+  {
+    title: "Getting Started",
+    slug: "getting-started3",
+    image: "getting-started.png",
+    excerpt: "NextJS is the React Framework for production",
+    date: "2024-04-16",
+  },
+  {
+    title: "Getting Started",
+    slug: "getting-started4",
+    image: "getting-started.png",
+    excerpt: "NextJS is the React Framework for production",
+    date: "2024-04-16",
+  },
+];
 
 const HomePage = () => {
   return (
     <>
       <Hero />
+      <FeaturedPosts posts={DUMMY_POSTS} />
     </>
   );
 };
