ceaa765a0054908e7ec10f9f38f8a0f87e279b3c
Ralph Woiwode <rawoiwode@gmail.com>
2024-09-27 09:39:59 -0500

    Adding react-markdown package and working on storing markdown files as blog posts

--- components/posts/post-detail/post-content.js
+++ components/posts/post-detail/post-content.js
@@ -1,3 +1,5 @@
+import ReactMarkdown from "react-markdown";
+
 import classes from "./post-content.module.css";
 import PostHeader from "./post-header";
 
@@ -9,13 +11,16 @@ const DUMMY_POST = {
   content: "# This is a first post",
 };
 
+/**
+ * Render Markdown to JSX with 'react-markdown' package
+ */
 const PostContent = () => {
   const imagePath = `/images/posts/${DUMMY_POST.slug}/${DUMMY_POST.image}`;
 
   return (
     <article className={classes.content}>
       <PostHeader title={DUMMY_POST.title} image={imagePath} />
-      {DUMMY_POST.content}
+      <ReactMarkdown>{DUMMY_POST.content}</ReactMarkdown>
     </article>
   );
 };
---
+++ content/posts/getting-started.md
@@ -0,0 +1,12 @@
+---
+# Grey Matter aka Meta Data
+title: Getting Started with NextJS
+date: "2024-10-01"
+image: getting-started.png
+excerpt: NextJS is the React Framework for production
+isFeatured: true
+---
+
+# This is a title
+
+This is some regular text with a [link](https://google.com)
