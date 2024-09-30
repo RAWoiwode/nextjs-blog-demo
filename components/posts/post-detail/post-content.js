a0c55d0856bf3746640ce201ad22ca8dfd8665ce
Ralph Woiwode <rawoiwode@gmail.com>
2024-09-30 09:44:15 -0500

    Customizing the elements displayed from markdown files

--- components/posts/post-detail/post-content.js
+++ components/posts/post-detail/post-content.js
@@ -1,4 +1,7 @@
-import ReactMarkdown from "react-markdown";
+import Markdown from "react-markdown";
+import Image from "next/image";
+import { Prism } from "react-syntax-highlighter";
+import { dracula } from "react-syntax-highlighter/dist/cjs/styles/prism";
 
 import classes from "./post-content.module.css";
 import PostHeader from "./post-header";
@@ -9,10 +12,52 @@ import PostHeader from "./post-header";
 const PostContent = ({ post }) => {
   const imagePath = `/images/posts/${post.slug}/${post.image}`;
 
+  const customRenderers = {
+    // img(image) {
+    //   return (
+    //     <Image
+    //       src={`/images/posts/${post.slug}/${image.src}`}
+    //       alt={image.alt}
+    //       width={600}
+    //       height={300}
+    //     />
+    //   );
+    // },
+    p: (paragraph) => {
+      const { node } = paragraph;
+
+      if (node.children[0].tagName === "img") {
+        const image = node.children[0];
+
+        return (
+          <div className={classes.image}>
+            <Image
+              src={`/images/posts/${post.slug}/${image.properties.src}`}
+              alt={image.properties.alt}
+              width={600}
+              height={300}
+            />
+          </div>
+        );
+      }
+
+      return <p>{paragraph.children}</p>;
+    },
+    code: (code) => {
+      const { className, children } = code;
+      const language = className.split("-")[1]; // className is something like language-js
+      return (
+        <Prism language={language} style={dracula}>
+          {children}
+        </Prism>
+      );
+    },
+  };
+
   return (
     <article className={classes.content}>
       <PostHeader title={post.title} image={imagePath} />
-      <ReactMarkdown>{post.content}</ReactMarkdown>
+      <Markdown components={customRenderers}>{post.content}</Markdown>
     </article>
   );
 };
